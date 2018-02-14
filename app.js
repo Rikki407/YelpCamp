bb702da05da426ebca0438e628b014cee5ec8dc0
Rikki407 <rishablamba407@gmail.com>
2018-02-14 23:49:46 +0530

    mongo db added

--- app.js
+++ app.js
@@ -1,77 +1,49 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express         = require("express"),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose");
+var app     = express();
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name : String,
+    image : String
+});
+
+var Campground = mongoose.model("Campground",campgroundSchema);
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
-var campgrounds = [
-    {
-        name : "Villasol",
-        image : "https://images.unsplash.com/photo-1482463084673-98272196658a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=457b7840cb35f335c04c8d37a7210a4d&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-        name : "Armanello",
-        image : "https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=44296710cae68fa09464afdbe49e6be8&auto=format&fit=crop&w=634&q=80"
-    },
-    {
-        name : "Arena Blanca",
-        image : "https://images.unsplash.com/photo-1460458248189-2cb101df4e67?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef0ed70802c5c63761e094167567d2fe&auto=format&fit=crop&w=1120&q=80"
-    },
-    {
-        name : "Beach Caravan",
-        image : "https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-0.3.5&s=f28297699deca72c5ba51af973adaa5c&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-        name : "Villasol",
-        image : "https://images.unsplash.com/photo-1482463084673-98272196658a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=457b7840cb35f335c04c8d37a7210a4d&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-        name : "Armanello",
-        image : "https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=44296710cae68fa09464afdbe49e6be8&auto=format&fit=crop&w=634&q=80"
-    },
-    {
-        name : "Arena Blanca",
-        image : "https://images.unsplash.com/photo-1460458248189-2cb101df4e67?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef0ed70802c5c63761e094167567d2fe&auto=format&fit=crop&w=1120&q=80"
-    },
-    {
-        name : "Beach Caravan",
-        image : "https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-0.3.5&s=f28297699deca72c5ba51af973adaa5c&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-        name : "Villasol",
-        image : "https://images.unsplash.com/photo-1482463084673-98272196658a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=457b7840cb35f335c04c8d37a7210a4d&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-        name : "Armanello",
-        image : "https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=44296710cae68fa09464afdbe49e6be8&auto=format&fit=crop&w=634&q=80"
-    },
-    {
-        name : "Arena Blanca",
-        image : "https://images.unsplash.com/photo-1460458248189-2cb101df4e67?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef0ed70802c5c63761e094167567d2fe&auto=format&fit=crop&w=1120&q=80"
-    },
-    {
-        name : "Beach Caravan",
-        image : "https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-0.3.5&s=f28297699deca72c5ba51af973adaa5c&auto=format&fit=crop&w=1050&q=80"
-    }
-];
 
 app.get("/",function (req, res) {
+
    res.render("landing");
 });
 
 app.get("/campgrounds",function (req, res) {
-
-    res.render("campgrounds",{campgrounds : campgrounds});
+    Campground.find({},function (err, allCampGrounds) {
+       if(err){
+           console.log(err);
+       } else{
+           res.render("campgrounds",{campgrounds : allCampGrounds});
+       }
+    });
 });
 app.post("/campgrounds",function (req, res) {
     var cg_name = req.body.name;
     var cg_img = req.body.image;
-    campgrounds.push({
+    Campground.create({
         name : cg_name,
         image : cg_img
+    },function (err , ground) {
+        if(err){
+            console.log(err);
+        }else {
+            res.redirect("/campgrounds");
+        }
     });
-    res.redirect("/campgrounds");
 });
 app.get("/campgrounds/new",function (req, res) {
    res.render("new");
