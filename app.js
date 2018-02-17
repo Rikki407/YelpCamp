e9d035957dd45a6be0a58a754b8aaea57726d048
Rikki407 <rishablamba407@gmail.com>
2018-02-17 12:26:16 +0530

    main.css v1 completed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app     = express();
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
 app.set("view engine","ejs");
 
 seedDB();
