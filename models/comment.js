e222218ebf21c06894d93f3d9c6ad532e7c335a8
Rikki407 <rishablamba407@gmail.com>
2018-02-18 11:58:56 +0530

    associations of comments with user

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,14 @@ var mongoose = require("mongoose");
 
 var commentSchema = new mongoose.Schema({
         text : String,
-        author : String
+        author : {
+
+            id : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : "users"
+            },
+            username : String
+        }
     });
 
 module.exports = mongoose.model("comments",commentSchema);
